85da0d0dd3550874f5a9b9b4965581d2602d20e0
avisek123 <mrperfectavi89@gmail.com>
2022-03-23 22:02:33 +0530

    whole data

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from '../features/counter/counterSlice';
 import todoSlice from "../features/todo/todoSlice";
+import { postApi } from "../services/post";
 export const store = configureStore({
     reducer: {
         counter: CounterReducer,
-        todoSlicer: todoSlice
+        todoSlicer: todoSlice,
+        [postApi.reducerPath]: postApi.reducer,
       
-    }
+    },
+    middleware:(getDefaultMiddleware)=>
+    getDefaultMiddleware().concat([postApi.middleware]),
 });
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
